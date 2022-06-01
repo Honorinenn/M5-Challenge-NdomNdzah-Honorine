4a84dff7ac9ce1f713a8c63c9d62117c267adb24
Honorine Ndom <honorinendzah@gmail.com>
2022-06-01 04:43:54 -0700

    added gamestorecatalog, invoicing and service registry

---
+++ gamestorecatalog/src/main/java/com/trilogyed/gamestorecatalog/GameStoreCatalogApplication.java
@@ -0,0 +1,15 @@
+package com.trilogyed.gamestorecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class GameStoreCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreCatalogApplication.class, args);
+	}
+
+}
