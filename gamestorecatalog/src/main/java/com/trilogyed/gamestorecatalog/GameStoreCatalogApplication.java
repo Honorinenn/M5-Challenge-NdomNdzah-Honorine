4bb2207700ccb2b94cb090ee7a8c5a4b2ad4eab2
Honorine Ndom <honorinendzah@gmail.com>
2022-06-03 05:36:57 -0700

    updated repository

--- gamestorecatalog/src/main/java/com/trilogyed/gamestorecatalog/GameStoreCatalogApplication.java
+++ gamestorecatalog/src/main/java/com/trilogyed/gamestorecatalog/GameStoreCatalogApplication.java
@@ -3,9 +3,8 @@ package com.trilogyed.gamestorecatalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@EnableConfigServer
 @SpringBootApplication
-
 public class GameStoreCatalogApplication {
 
 	public static void main(String[] args) {
--- gamestoreinvoicing/src/main/java/com/trilogyed/gamestore/GameStoreInvoicingApplication.java
+++ gamestoreinvoicing/src/main/java/com/trilogyed/gamestore/GameStoreInvoicingApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GameStoreApplication {
+@EnableDiscoveryClient
+public class GameStoreInvoicingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GameStoreApplication.class, args);
+		SpringApplication.run(GameStoreInvoicingApplication.class, args);
 	}
 
 }
